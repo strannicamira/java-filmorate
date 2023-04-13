90e8ba362b72d67962822efd60e76cd1004b13ac
strannicamira <strannicamira@yandex.ru>
2023-04-13 11:18:10 +0300

    Add model

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+@Data
+public class Film {
+    private final Integer id;
+    private String name;
+    private String description;
+    private final LocalDate releaseDate;
+    private final Duration duration;
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private final Integer id;
+    private String email;
+    private String login;
+    private String name;
+    private final LocalDate birthday;
+}
