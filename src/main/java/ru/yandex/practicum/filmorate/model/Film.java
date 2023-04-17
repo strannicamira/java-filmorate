1526f55ae3d1918afcb6739957af04564bab6deb
strannicamira <strannicamira@yandex.ru>
2023-04-17 14:26:53 +0300

    Update for checkstyle test

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,10 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
-import lombok.Data;
 import lombok.Value;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Positive;
-import java.time.Duration;
 import java.time.LocalDate;
 
 @Value
