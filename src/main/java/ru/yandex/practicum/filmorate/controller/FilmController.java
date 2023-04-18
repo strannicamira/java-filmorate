7b3abe2f8fc84d812e984580723a963ac47e4d1a
strannicamira <strannicamira@yandex.ru>
2023-04-18 15:34:58 +0300

    Apply comments

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -8,14 +8,14 @@ import javax.validation.Valid;
 import javax.validation.ValidationException;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @Slf4j
 public class FilmController {
 
-    private HashMap<Integer, Film> films = new HashMap<>();
+    private ConcurrentHashMap<Integer, Film> films = new ConcurrentHashMap<>();
     private Integer idCounter = 0;
 
     @GetMapping("/films")
@@ -29,6 +29,7 @@ public class FilmController {
         if (film.getDescription().length() > 200 ||
                 film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))
         ) {
+            log.warn("Фильм не создан. Не прошел проверку: '{}'", film);
             throw new ValidationException();
         }
         Film resultFilm = Film.builder()
@@ -50,6 +51,7 @@ public class FilmController {
             films.put(film.getId(), film);
             log.debug("Фильм обновлен: '{}'", film);
         } else {
+            log.warn("Фильм не обновлен. Не найден в списке: '{}'", film);
             throw new ValidationException();
         }
         return film;
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,20 +1,21 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
 import javax.validation.ValidationException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @Slf4j
 public class UserController {
 
-    private HashMap<Integer, User> users = new HashMap<>();
+    private ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
     private Integer idCounter = 0;
 
     @GetMapping("/users")
@@ -25,11 +26,9 @@ public class UserController {
     @PostMapping(value = "/users")
     @ResponseBody
     public User create(@Valid @RequestBody User user) {
-        String userName;
-        if (user.getName() == null || user.getName().isEmpty()) {
+        String userName = user.getName();
+        if (!StringUtils.hasText(userName)) {
             userName = user.getLogin();
-        } else {
-            userName = user.getName();
         }
         final User resultUser = User.builder()
                 .id(++idCounter)
@@ -50,6 +49,8 @@ public class UserController {
             users.put(user.getId(), user);
             log.debug("Пользователь обновлен: '{}'", user);
         } else {
+            log.warn("Пользователь не обновлен. Не найден в списке: '{}'",
+                    user);
             throw new ValidationException();
         }
         return user;
