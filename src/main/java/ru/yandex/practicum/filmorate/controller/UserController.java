08d098506b9cb79bf1c60762cde7ea9dc32d7fe2
strannicamira <strannicamira@yandex.ru>
2023-05-10 12:28:54 +0300

    Correct Checkstyle for PR tests (2)

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -34,30 +34,30 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public User getUser(@PathVariable("id") Integer id){
+    public User getUser(@PathVariable("id") Integer id) {
         return userService.findUserById(id);
     }
 
     @PutMapping(value = "/users/{id}/friends/{friendId}")
     public boolean addFriend(@PathVariable("id") Integer userId,
-                       @PathVariable("friendId") Integer friendId) {
-        return userService.addFriend(userId,friendId);
+                             @PathVariable("friendId") Integer friendId) {
+        return userService.addFriend(userId, friendId);
     }
 
     @DeleteMapping(value = "/users/{id}/friends/{friendId}")
     public boolean deleteFriend(@PathVariable("id") Integer userId,
-                          @PathVariable("friendId") Integer friendId) {
-        return userService.deleteFriend(userId,friendId);
+                                @PathVariable("friendId") Integer friendId) {
+        return userService.deleteFriend(userId, friendId);
     }
 
     @GetMapping("/users/{id}/friends")
-    public List<User> getFriends(@PathVariable("id") Integer userId){
+    public List<User> getFriends(@PathVariable("id") Integer userId) {
         return userService.getFriends(userId);
     }
 
     @GetMapping(value = "/users/{id}/friends/common/{otherId}")
     public List<User> getCommonFriend(@PathVariable("id") Integer id,
                                       @PathVariable("otherId") Integer otherId) {
-        return userService.getCommonFriends(id,otherId);
+        return userService.getCommonFriends(id, otherId);
     }
 }
