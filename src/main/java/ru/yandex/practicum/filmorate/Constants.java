1455bc23a1d29e9ece3c91d46e174ebf4470daad
strannicamira <strannicamira@yandex.ru>
2023-05-11 21:16:40 +0300

    Correct Checkstyle for PR tests (3.1)

--- src/main/java/ru/yandex/practicum/filmorate/Constants.java
+++ src/main/java/ru/yandex/practicum/filmorate/Constants.java
@@ -3,8 +3,8 @@ package ru.yandex.practicum.filmorate;
 import java.util.Set;
 
 public class Constants {
-    public static final String DESCENDING_ORDER = "desc";
     public static final String ASCENDING_ORDER = "asc";
+    public static final String DESCENDING_ORDER = "desc";
 
     public static final Set<String> SORTS = Set.of(ASCENDING_ORDER, DESCENDING_ORDER);
 }
