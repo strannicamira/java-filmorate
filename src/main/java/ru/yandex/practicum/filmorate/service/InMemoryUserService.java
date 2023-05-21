0e81a08e0dc5977d8d0d9719cc222bfe080168fe
strannicamira <strannicamira@yandex.ru>
2023-05-21 16:21:00 +0300

    Correct for some checks 1: User get friends common empty and so on

--- src/main/java/ru/yandex/practicum/filmorate/service/InMemoryUserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/InMemoryUserService.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
@@ -20,36 +21,58 @@ public class InMemoryUserService implements UserService {
 
     @Override
     public boolean addFriend(Integer userId, Integer friendId) {
-        return userStorage.getUsers().get(userId).getFriends().add(friendId) &&
-                userStorage.getUsers().get(friendId).getFriends().add(userId);
+        Map<Integer, User> users = userStorage.getUsers();
+        if (userId != null && users.containsKey(userId) && friendId != null && users.containsKey(friendId)) {
+            return users.get(userId).getFriends().add(friendId) &&
+                    users.get(friendId).getFriends().add(userId);
+        } else {
+            throw new NotFoundException("Пользователь найден в списке.");
+        }
     }
 
     @Override
     public boolean deleteFriend(Integer userId, Integer friendId) {
-        return userStorage.getUsers().get(userId).getFriends().remove(friendId) &&
-                userStorage.getUsers().get(friendId).getFriends().remove(userId);
+        Map<Integer, User> users = userStorage.getUsers();
+        if (userId != null && users.containsKey(userId) && friendId != null && users.containsKey(friendId)) {
+            return users.get(userId).getFriends().remove(friendId) &&
+                    users.get(friendId).getFriends().remove(userId);
+        } else {
+            throw new NotFoundException("Пользователь найден в списке.");
+        }
+
     }
 
     @Override
     public List<User> getFriends(Integer userId) {
         List<User> friends = new ArrayList<>();
-        for (Integer id : userStorage.getUsers().get(userId).getFriends()) {
-            friends.add(userStorage.getUsers().get(id));
+        Map<Integer, User> users = userStorage.getUsers();
+        if (userId != null && users.containsKey(userId)) {
+            for (Integer id : users.get(userId).getFriends()) {
+                friends.add(users.get(id));
+            }
+            return friends;
+        } else {
+            throw new NotFoundException("Пользователь не найден в списке.");
         }
-        return friends;
     }
 
     @Override
     public List<User> getCommonFriends(Integer userId, Integer otherId) {
         List<User> friends = new ArrayList<>();
-        for (Integer id : userStorage.getUsers().get(userId).getFriends()) {
-            for (Integer other :
-                    userStorage.getUsers().get(otherId).getFriends())
-                if (id == other) {
-                    friends.add(userStorage.getUsers().get(id));
-                }
+        Map<Integer, User> users = userStorage.getUsers();
+        if (userId != null && users.containsKey(userId) && otherId != null && users.containsKey(otherId)) {
+            for (Integer id : userStorage.getUsers().get(userId).getFriends()) {
+                for (Integer other :
+                        userStorage.getUsers().get(otherId).getFriends())
+                    if (id == other) {
+                        friends.add(userStorage.getUsers().get(id));
+                    }
+            }
+            return friends;
+        } else {
+            throw new NotFoundException("Пользователь найден в списке.");
         }
-        return friends;
+
     }
 
     @Override
